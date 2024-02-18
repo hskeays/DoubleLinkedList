462c9866e32414262847732ef7bce81faa338aa5
hskeays <hskeays@gmail.com>
2024-02-17 19:04:36 -0500

    linkedlist

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
-
-import java.util.LinkedList;
+import java.util.*;
 
 class Main {
     public static void main(String[] args) {
+
         LinkedList<Integer> linkedList = new LinkedList<>();
 
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7};
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8};
         for (int num : numbers) {
             linkedList.add(num);
         }
+
         System.out.println(linkedList);
 
     }
